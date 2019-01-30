04c26457385083fa2ef7e21e205b835e621f65a4
foxidw <3032152997@qq.com>
2019-01-30 10:02:02 +0800

    modified

---
+++ myh5-master/002Console/alert.js
@@ -0,0 +1,10 @@
+function taolu(){
+    alert("子曰：\n“ly是天才天才”");
+    alert("子曰：\n“没错”");
+    alert("子曰：\n“是的”");
+}
+alert("子曰：\n“========ly是天才天才”");
+alert("你相信奇迹吗 ");
+alert("并不啊");
+taolu();
+alert("子曰：\n“===========没错”");
